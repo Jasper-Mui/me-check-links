154c7aa036c517974e38e833e43534b4c65c0317
unknown <jaspermui@hotmail.ca>
2020-10-12 06:11:44 -0400

    feature good/bad flags

--- src/main.js
+++ src/main.js
@@ -10,17 +10,21 @@ function parseArgs(rawArgs) {
     const args = arg(
       {
         '-v': Boolean,
-        '-u': Boolean
+        '-u': Boolean,
+        '-g': Boolean,
+        '-b': Boolean,
       }
     );
     return {
       version: args['-v'] || false,
       url: args['-u'] || false,
-      inputArg: args['-u'] ? rawArgs.slice(3) : rawArgs.slice(2)
+      inputArg: args['-u'] || args['-g'] || args['-b'] ? rawArgs.slice(3) : rawArgs.slice(2),
+      showGood: args['-g'] || false,
+      showBad: args['-b'] || false,
     };
 }
 
-function linkCheck(file) {
+function linkCheck(file, showGood, showBad) {
     const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,25}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g
     const urls = file.match(regex);
 
@@ -28,11 +32,11 @@ function linkCheck(file) {
         fetch(url)
             .then(res => {
                 if (res.ok) {                
-                    if (res.status == 200) {
+                    if (res.status == 200 && showGood) {
                         console.log(chalk.green(url));
-                    } else if (res.status == 400 || res.status == 404) {
+                    } else if ((res.status == 400 || res.status == 404) && showBad) {
                         console.log(chalk.red(url));
-                    } else {
+                    } else if (showBad) {
                         console.log(chalk.grey(url));
                     }
                 } else {
@@ -40,13 +44,20 @@ function linkCheck(file) {
                 }
             })
             .catch(error => {
-                console.log(chalk.red(url));
+                if (showBad)
+                    console.log(chalk.red(url));
             });
     })
 }
 
 export function cli(args) {
     const parsedArgs = parseArgs(args)
+    console.log(parsedArgs)
+
+    if (!(parsedArgs.showGood || parsedArgs.showBad)){
+        parsedArgs.showGood = true
+        parsedArgs.showBad = true
+    } 
 
     if ((!parseArgs.version && !parseArgs.url && parsedArgs.inputArg == 0) || (parseArgs.version && parseArgs.url)){
         console.log("mcl - a command to check links of a HTML page")
@@ -63,7 +74,7 @@ export function cli(args) {
                     }
                     return res.text();
                 })
-                .then(body => linkCheck(body))
+                .then(body => linkCheck(body, parsedArgs.showGood, parsedArgs.showBad))
                 .catch((error) => {
                     console.error('Fetch operation failed', error);
                     console.log(chalk.bgRed("Invalid url, absolute URL only"));
@@ -73,7 +84,7 @@ export function cli(args) {
         parsedArgs.inputArg.map(file => {
             fs.readFile(file, (err, data) => {
                 if (err) throw err;
-                linkCheck(data.toString());
+                linkCheck(data.toString(), parsedArgs.showGood, parsedArgs.showBad);
             })
         })
     }
